0ea2dad2755907f4aad0fade67d600e2f510d305
Deepak Moud <deepakmoud@poornima.org>
2021-09-01 14:11:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
 from flask.helpers import send_file
 from jinja2 import Template
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+
+
 import matplotlib.pyplot as plt
 from os import path
 import re
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 import os
-from sklearn.preprocessing import LabelEncoder
-from sklearn.impute import SimpleImputer
+
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
-import pandas as pd
-import numpy as np
-import matplotlib
-import numpy as np
+
+
 from flask import Flask, request, jsonify, render_template
 
-import pandas as pd
+
 
 # coding=utf-8
 import sys
